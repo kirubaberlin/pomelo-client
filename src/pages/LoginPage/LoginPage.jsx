f25cc835ca5d7445795b49024b3b5070b563459d
Bright Mensah <brightmensah064@gmail.com>
2023-10-14 13:54:19 +0000

    created the profile page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -20,14 +20,25 @@ function LoginPage() {
     e.preventDefault();
     const requestBody = { email, password };
 
+    // Send a request to the server using axios
+    /* 
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/auth/login`)
+      .then((response) => {})
+    */
+
+    // Or using a service
     authService
       .login(requestBody)
       .then((response) => {
+        // If the POST request is successful store the authentication token,
+        // after the token is stored authenticate the user
+        // and at last navigate to the home page
         storeToken(response.data.authToken);
         authenticateUser();
         navigate("/");
       })
       .catch((error) => {
+        // If the request resolves with an error, set the error message in the state
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
       });
@@ -35,36 +46,26 @@ function LoginPage() {
 
   return (
     <div className="LoginPage">
-      <div className="image-container">
-        <img src="your-image-url.jpg" alt="Login" />
-      </div>
-      <div className="login-container">
-        <h1>Login</h1>
-        <form className="login-form" onSubmit={handleLoginSubmit}>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleEmail}
-          />
+      <h1>Login</h1>
 
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handlePassword}
-          />
+      <form onSubmit={handleLoginSubmit}>
+        <label>Email:</label>
+        <input type="email" name="email" value={email} onChange={handleEmail} />
 
-          <button className="login-button" type="submit">
-            Login
-          </button>
-        </form>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <p>Don't have an account yet?</p>
-        <Link to={"/signup"}> Sign Up</Link>
-      </div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Don't have an account yet?</p>
+      <Link to={"/signup"}> Sign Up</Link>
     </div>
   );
 }
